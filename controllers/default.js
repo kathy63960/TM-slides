0ec3b9d31c3b683a75e3e44b8ed829aa9a5ab9f6
Herve Prot <herve.prot@tomanage.fr>
2016-11-04 22:50:55 +0100

    Fix noindex, nofollow

--- controllers/default.js
+++ controllers/default.js
@@ -63,8 +63,6 @@ function file_read(req, res) {
 	// Custom image resizing
 	var size;
 
-	var size;
-
 	// Small hack for the file cache.
 	// F.exists() uses req.uri.pathname for creating temp identificator and skips all query strings by creating (because this hack).
 	if (req.query.s) {
